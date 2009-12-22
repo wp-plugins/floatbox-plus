604bc71ef8b03433d49f1c6d80f11e3fde01f13f
cybio <cybio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-22 17:36:46 +0000

    r234@forge (orig r34):  olli | 2009-06-13 23:09:00 +0200  - [FIX] WP <2.8 compatibility/enqueue js/cssfiles
    git-svn-id: https://plugins.svn.wordpress.org/floatbox-plus/trunk@186199 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- floatbox-plus.php
+++ floatbox-plus.php
@@ -1316,6 +1316,14 @@ class floatbox_plus {
         }
     }
 
+    function enqueueAdmin($hook_suffix) {
+        // print '<!-- enqueueAdmin: '.$hook_suffix.' -->';
+        $fbp_admin_pages = array('post-new.php', 'post.php');
+        if(in_array($hook_suffix, $fbp_admin_pages)) {
+            wp_enqueue_script('wp-polls-admin', plugins_url('/floatbox-plus/floatbox-plus.js'), null , $this->version, true);
+        }
+    }
+
 	function direct_image_urls_for_galleries( $link, $id ) {
 		if ( is_admin() ) return $link;
 
